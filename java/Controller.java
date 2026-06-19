483a94831e046e9c3c0ffda7661514fcb7f2351a
Arceus27 <daveramanantsoa@gmail.com>
2026-06-19 11:55:23 +0300

    frontcontroller
    detetion de classes avec annotation

--- java/Controller.java
+++ java/Controller.java
@@ -1,5 +1,4 @@
 package  com.app.annotations;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
--- java/FrontControllerServlet.java
+++ java/FrontControllerServlet.java
@@ -1,29 +1,86 @@
+import com.app.annotations.Controller;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import java.io.*;
+import java.net.URL;
+import java.util.*;
 
 public class FrontControllerServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    
+    private List<String> nomsClasses = new ArrayList<>();
+    
+    @Override
+    public void init() throws ServletException {
+        try {
+            String nomPackage = "com.app.controllers";
+            String cheminPackage = nomPackage.replace(".", "/");
+            
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            Enumeration<URL> resources = classLoader.getResources(cheminPackage);
+            
+            while (resources.hasMoreElements()) {
+                URL resource = resources.nextElement();
+                
+                if (resource.getProtocol().equals("file")) {
+                    File directory = new File(resource.getFile());
+                    
+                    if (directory.exists()) {
+                        for (File fichier : directory.listFiles()) {
+                            if (fichier.getName().endsWith(".class")) {
+                                String nomClasse = nomPackage + "." + 
+                                    fichier.getName().replace(".class", "");
+                                
+                                Class<?> clazz = Class.forName(nomClasse);
+                                
+                                if (clazz.isAnnotationPresent(Controller.class)) {
+                                    nomsClasses.add(clazz.getSimpleName());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
         processRequest(request, response);
     }
     
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
         processRequest(request, response);
     }
     
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String uri = request.getRequestURI();
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        out.println("<!DOCTYPE html>");
-        out.println("<html lang=\"en\">");
-        out.println("<head>");
-        out.println("<smeta charset=\"UTF-8\">");
-        out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
-        out.println("<title>Document</title>");
-        out.println("</head>");
+        
+        out.println("<html>");
+        out.println("<head><title>Front Controller</title></head>");
         out.println("<body>");
         out.println("<h1>Front Controller</h1>");
-        out.println("<p>URI:bonjour depuis le controlleur</p>");
+       
+        out.println("<h2>Classes avec @Controller :</h2>");
+        out.println("<ul>");
+        
+        if (nomsClasses.isEmpty()) {
+            out.println("<li>Aucune classe avec @Controller</li>");
+        } else {
+            for (String nom : nomsClasses) {
+                out.println("<li>" + nom + "</li>");
+            }
+        }
+        
+        out.println("</ul>");
+        out.println("<p>Total: " + nomsClasses.size() + " classe(s)</p>");
         out.println("</body>");
         out.println("</html>");
     }
